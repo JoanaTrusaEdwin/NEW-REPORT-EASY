881f3ff1e8a9899b6c7f262c930b0f4c898ac3c5
shalini ravindran <bcs21030003@student.uts.edu.my>
2023-11-02 10:01:39 +0800

    models added

---
+++ NEW-REPORT-EASY/Models/IncidentService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NEW_REPORT_EASY.Models
+public class IncidentService
+{
+    private readonly AppDbContext _context;
+
+    public IncidentService(AppDbContext context)
+    {
+        _context = context;
+    }
+}
+
+
---
+++ NEW-REPORT-EASY/Models/UserProfile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace NEW_REPORT_EASY.Models
+
+public class UserProfile
+{
+    public int Id { get; set; }
+    public string Email { get; set; }
+    public string Username { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string PasswordHash { get; set; }
+    public string IdentificationNumber { get; set; }
+
+}
+
